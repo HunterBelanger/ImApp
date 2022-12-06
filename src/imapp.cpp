83c4c06208a140886cab82d6b87279029a11a936
Hunter Belanger <hunter.belanger@gmail.com>
2022-12-06 11:24:36 -0500

    Eliminate some conversion warnings from MSVC

--- src/imapp.cpp
+++ src/imapp.cpp
@@ -309,110 +309,111 @@ void App::set_default_style() {
   style_->SelectableTextAlign = ImVec2(0.0, 0.0);
 
   ImVec4* Colors = style_->Colors;
-  Colors[ImGuiCol_Text] = ImVec4(1.0, 1.0, 1.0, 1.0);
-  Colors[ImGuiCol_TextDisabled] =
-      ImVec4(0.5921568870544434, 0.5921568870544434, 0.5921568870544434, 1.0);
-  Colors[ImGuiCol_WindowBg] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_ChildBg] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_PopupBg] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_Border] =
-      ImVec4(0.3058823645114899, 0.3058823645114899, 0.3058823645114899, 1.0);
-  Colors[ImGuiCol_BorderShadow] =
-      ImVec4(0.3058823645114899, 0.3058823645114899, 0.3058823645114899, 1.0);
-  Colors[ImGuiCol_FrameBg] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
-  Colors[ImGuiCol_FrameBgHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
+  Colors[ImGuiCol_Text] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+  Colors[ImGuiCol_TextDisabled] = ImVec4(
+      0.5921568870544434f, 0.5921568870544434f, 0.5921568870544434f, 1.0f);
+  Colors[ImGuiCol_WindowBg] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                     0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_ChildBg] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                    0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_PopupBg] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                    0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_Border] = ImVec4(0.3058823645114899f, 0.3058823645114899f,
+                                   0.3058823645114899f, 1.0f);
+  Colors[ImGuiCol_BorderShadow] = ImVec4(
+      0.3058823645114899f, 0.3058823645114899f, 0.3058823645114899f, 1.0f);
+  Colors[ImGuiCol_FrameBg] = ImVec4(0.2000000029802322f, 0.2000000029802322f,
+                                    0.2156862765550613f, 1.0f);
+  Colors[ImGuiCol_FrameBgHovered] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
   Colors[ImGuiCol_FrameBgActive] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_TitleBg] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_TitleBgActive] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_TitleBgCollapsed] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_MenuBarBg] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
-  Colors[ImGuiCol_ScrollbarBg] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_TitleBg] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                    0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_TitleBgActive] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_MenuBarBg] = ImVec4(0.2000000029802322f, 0.2000000029802322f,
+                                      0.2156862765550613f, 1.0f);
+  Colors[ImGuiCol_ScrollbarBg] = ImVec4(
+      0.2000000029802322f, 0.2000000029802322f, 0.2156862765550613f, 1.0f);
   Colors[ImGuiCol_ScrollbarGrab] =
-      ImVec4(0.321568638086319, 0.321568638086319, 0.3333333432674408, 1.0);
-  Colors[ImGuiCol_ScrollbarGrabHovered] =
-      ImVec4(0.3529411852359772, 0.3529411852359772, 0.3725490272045135, 1.0);
-  Colors[ImGuiCol_ScrollbarGrabActive] =
-      ImVec4(0.3529411852359772, 0.3529411852359772, 0.3725490272045135, 1.0);
+      ImVec4(0.321568638086319f, 0.321568638086319f, 0.3333333432674408f, 1.0);
+  Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(
+      0.3529411852359772f, 0.3529411852359772f, 0.3725490272045135f, 1.0f);
+  Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(
+      0.3529411852359772f, 0.3529411852359772f, 0.3725490272045135f, 1.0f);
   Colors[ImGuiCol_CheckMark] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_SliderGrab] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_SliderGrab] = ImVec4(0.1137254908680916f, 0.5921568870544434f,
+                                       0.9254902005195618f, 1.0f);
   Colors[ImGuiCol_SliderGrabActive] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_Button] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
-  Colors[ImGuiCol_ButtonHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
-  Colors[ImGuiCol_ButtonActive] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
-  Colors[ImGuiCol_Header] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
-  Colors[ImGuiCol_HeaderHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_Button] = ImVec4(0.2000000029802322f, 0.2000000029802322f,
+                                   0.2156862765550613f, 1.0f);
+  Colors[ImGuiCol_ButtonHovered] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
+  Colors[ImGuiCol_ButtonActive] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
+  Colors[ImGuiCol_Header] = ImVec4(0.2000000029802322f, 0.2000000029802322f,
+                                   0.2156862765550613f, 1.0f);
+  Colors[ImGuiCol_HeaderHovered] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
   Colors[ImGuiCol_HeaderActive] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_Separator] =
-      ImVec4(0.3058823645114899, 0.3058823645114899, 0.3058823645114899, 1.0);
-  Colors[ImGuiCol_SeparatorHovered] =
-      ImVec4(0.3058823645114899, 0.3058823645114899, 0.3058823645114899, 1.0);
-  Colors[ImGuiCol_SeparatorActive] =
-      ImVec4(0.3058823645114899, 0.3058823645114899, 0.3058823645114899, 1.0);
-  Colors[ImGuiCol_ResizeGrip] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_ResizeGripHovered] =
-      ImVec4(0.2000000029802322, 0.2000000029802322, 0.2156862765550613, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_Separator] = ImVec4(0.3058823645114899f, 0.3058823645114899f,
+                                      0.3058823645114899f, 1.0f);
+  Colors[ImGuiCol_SeparatorHovered] = ImVec4(
+      0.3058823645114899f, 0.3058823645114899f, 0.3058823645114899f, 1.0f);
+  Colors[ImGuiCol_SeparatorActive] = ImVec4(
+      0.3058823645114899f, 0.3058823645114899f, 0.3058823645114899f, 1.0f);
+  Colors[ImGuiCol_ResizeGrip] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                       0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_ResizeGripHovered] = ImVec4(
+      0.2000000029802322f, 0.2000000029802322f, 0.2156862765550613f, 1.0f);
   Colors[ImGuiCol_ResizeGripActive] =
-      ImVec4(0.321568638086319, 0.321568638086319, 0.3333333432674408, 1.0);
-  Colors[ImGuiCol_Tab] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_TabHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
+      ImVec4(0.321568638086319f, 0.321568638086319f, 0.3333333432674408f, 1.0f);
+  Colors[ImGuiCol_Tab] = ImVec4(0.1450980454683304f, 0.1450980454683304f,
+                                0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_TabHovered] = ImVec4(0.1137254908680916f, 0.5921568870544434f,
+                                       0.9254902005195618f, 1.0f);
   Colors[ImGuiCol_TabActive] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_TabUnfocused] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_TabUnfocused] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
   Colors[ImGuiCol_TabUnfocusedActive] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
   Colors[ImGuiCol_PlotLines] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_PlotLinesHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_PlotLinesHovered] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
   Colors[ImGuiCol_PlotHistogram] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_PlotHistogramHovered] =
-      ImVec4(0.1137254908680916, 0.5921568870544434, 0.9254902005195618, 1.0);
-  Colors[ImGuiCol_TableHeaderBg] =
-      ImVec4(0.1882352977991104, 0.1882352977991104, 0.2000000029802322, 1.0);
-  Colors[ImGuiCol_TableBorderStrong] =
-      ImVec4(0.3098039329051971, 0.3098039329051971, 0.3490196168422699, 1.0);
-  Colors[ImGuiCol_TableBorderLight] =
-      ImVec4(0.2274509817361832, 0.2274509817361832, 0.2470588237047195, 1.0);
-  Colors[ImGuiCol_TableRowBg] = ImVec4(0.0, 0.0, 0.0, 0.0);
-  Colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.0, 1.0, 1.0, 0.05999999865889549);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(
+      0.1137254908680916f, 0.5921568870544434f, 0.9254902005195618f, 1.0f);
+  Colors[ImGuiCol_TableHeaderBg] = ImVec4(
+      0.1882352977991104f, 0.1882352977991104f, 0.2000000029802322f, 1.0f);
+  Colors[ImGuiCol_TableBorderStrong] = ImVec4(
+      0.3098039329051971f, 0.3098039329051971f, 0.3490196168422699f, 1.0f);
+  Colors[ImGuiCol_TableBorderLight] = ImVec4(
+      0.2274509817361832f, 0.2274509817361832f, 0.2470588237047195f, 1.0f);
+  Colors[ImGuiCol_TableRowBg] = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+  Colors[ImGuiCol_TableRowBgAlt] =
+      ImVec4(1.0f, 1.0f, 1.0f, 0.05999999865889549f);
   Colors[ImGuiCol_TextSelectedBg] =
-      ImVec4(0.0, 0.4666666686534882, 0.7843137383460999, 1.0);
-  Colors[ImGuiCol_DragDropTarget] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
-  Colors[ImGuiCol_NavHighlight] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
+      ImVec4(0.0f, 0.4666666686534882f, 0.7843137383460999f, 1.0f);
+  Colors[ImGuiCol_DragDropTarget] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
+  Colors[ImGuiCol_NavHighlight] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
   Colors[ImGuiCol_NavWindowingHighlight] =
-      ImVec4(1.0, 1.0, 1.0, 0.699999988079071);
+      ImVec4(1.0f, 1.0f, 1.0f, 0.699999988079071f);
   Colors[ImGuiCol_NavWindowingDimBg] =
-      ImVec4(0.800000011920929, 0.800000011920929, 0.800000011920929,
-             0.2000000029802322);
-  Colors[ImGuiCol_ModalWindowDimBg] =
-      ImVec4(0.1450980454683304, 0.1450980454683304, 0.1490196138620377, 1.0);
+      ImVec4(0.800000011920929f, 0.800000011920929f, 0.800000011920929f,
+             0.2000000029802322f);
+  Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(
+      0.1450980454683304f, 0.1450980454683304f, 0.1490196138620377f, 1.0f);
 }
 
 Image Image::from_file(const std::filesystem::path& fname) {
